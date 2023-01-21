9c279f6c624abed93c3cce84fa6bb516088dde45
Héctor Airy <hello@hectorairy.com>
2023-01-21 13:51:36 -0600

    require packages in project

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const fs = require("fs");
+
 console.clear();
 console.log("================");
 console.log("  Tabla del 5  ");
 console.log("================");
 
 const base = 5;
+let tabla = "";
 
 for (let i = 1; i <= 10; i++) {
-  console.log(`${base} X ${i} = ${base * i}`);
+  tabla += `${base} X ${i} = ${base * i}\n`;
 }
+
+console.log(tabla);
+
+fs.writeFile(`tabla-${base}.txt`, tabla, (err) => {
+  if (err) throw err;
+  console.log("archivo creado!");
+});
