aa3eaca0386df37c62f80a4a9483e62372439319
Héctor Airy <hello@hectorairy.com>
2023-01-21 20:19:12 -0600

    get data sent by console

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const { crearArchivo } = require("./helpers/multiplicar");
 console.clear();
-const base = 10;
+// const base = 10;
+
+// Puedes recibir datos que te envian por consola
+// pero esta forma de obtener los datos
+// ! no es recomendable
+const [, , arg3 = "base=5"] = process.argv;
+const [, base = 5] = arg3.split("=");
 
 crearArchivo(base)
   .then((nombreArchivo) => console.log(nombreArchivo, " creado"))
